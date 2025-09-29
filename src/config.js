c65bddf78321d2071d28f5b86ed18d17347dceb0
David Zarzavilla Borrego <dzarzavillaborrego@uma.es>
2025-09-30 00:29:18 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -5,9 +5,11 @@
 // // Google OAuth 2.0 Client ID (para Google Sign-In)
 // export const GOOGLE_CLIENT_ID = "59175635297-n0kjrvuu0ejccf3l857n5mmknn7iq9fo.apps.googleusercontent.com";
 
-export const SHEET_ID = process.env.REACT_APP_SHEET_ID;
-export const SHEET_NAME = process.env.REACT_APP_SHEET_NAME;
-export const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
-console.log("SHEET_ID:", process.env.REACT_APP_SHEET_ID);
-console.log("SHEET_NAME:", process.env.REACT_APP_SHEET_NAME);
-console.log("GOOGLE_CLIENT_ID:", process.env.REACT_APP_GOOGLE_CLIENT_ID);
+export const SHEET_ID = import.meta.env.VITE_SHEET_ID;
+export const SHEET_NAME = import.meta.env.VITE_SHEET_NAME;
+export const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
+console.log("SHEET_ID:", SHEET_ID);
+console.log("SHEET_NAME:", SHEET_NAME);
+console.log("GOOGLE_CLIENT_ID:", GOOGLE_CLIENT_ID);
+
